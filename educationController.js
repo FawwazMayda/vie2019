312cd1d2bc7d1a99dff7858b0d1d8f6959665e7a
fawwazmayda <maydafawwaz@gmail.com>
2019-11-16 13:37:30 +0700

    Tambahan derajat sekolah

--- educationController.js
+++ educationController.js
@@ -2,8 +2,12 @@ var express = require('express')
 var router = express.Router()
 var edu = require('./education')
 var bodyParser = require('body-parser')
+var kota = require('./city')
 router.use(bodyParser.json())
 
+function cityPercentageOnyear(resultArr){
+    
+}
 
 router.get("/",(req,resp)=>{
     resp.status(200).send({status:"OK"})
@@ -17,7 +21,12 @@ router.get("/:year",(req,res)=>{
 })
 
 router.get('/:year/:kota',(req,res)=>{
+    let tahun = req.params.year
+    let nama_kota = req.params.kota
     res.send("year is "+req.params.year+" kota is "+req.params.kota)
+    edu.find({tahun:tahun, kota:nama_kota},(err,d)=>{
+        res.status(200).send(d)
+    })
 })
 
 module.exports = router
