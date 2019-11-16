12a625560c5c3b82876cec7540a5459061ea0d22
fawwazmayda <maydafawwaz@gmail.com>
2019-11-16 22:39:29 +0700

    Fuck Up With Array.push()

--- educationController2.js
+++ educationController2.js
@@ -35,14 +35,44 @@ function pieChartData(resultArr){
     return data
 }
 
+function barChartData(resultArr){
+    let data = new Array()
+    kota.forEach(nama_kota => {
+        let d = {}
+        d['group']=nama_kota
+        derajat.forEach(sekolah => {
+            let jumlah = 0
+            d['category']=sekolah
+            resultArr.forEach(element => {
+                if(element['kota']==nama_kota){
+                    jumlah += element[sekolah] 
+                }
+            })
+            d['measure']=jumlah
+            console.log(d)
+            data.push({group:d['group'], category:d['category'], measure:d['measure']})
+        })
+
+    })
+    console.log("****************")
+    console.log(data)
+    return data
+}
 router.get("/",(req,res)=>{
     res.status(200).send({status:"OK"})
 })
 
-router.get("/:year",(req,res)=>{
+router.get("/piechart/:year",(req,res)=>{
     let tahun = req.params.year
     edu.find({tahun:tahun},(err,d)=>{
         res.status(200).send(pieChartData(d))
+    })
+})
+
+router.get("/barchart/:year/",(req,res)=>{
+    let tahun = req.params.year
+    edu.find({tahun:tahun},(err,d)=>{
+        res.status(200).send(barChartData(d))
     })
 })
 
