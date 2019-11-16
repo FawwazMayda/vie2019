630cbb5ee882dd1a02579b04ce3e1851f703e5af
fawwazmayda <maydafawwaz@gmail.com>
2019-11-16 13:15:40 +0700

    year Params

--- educationController.js
+++ educationController.js
@@ -9,4 +9,8 @@ router.get("/",(req,resp)=>{
     resp.status(200).send({status:"OK"})
 })
 
+router.get("/:year",(req,res)=>{
+    res.send("year is "+req.params.year)
+})
+
 module.exports = router
